2978bfd11da4351152d066249ccd5c73e8e070ee
Massimiliano Fanciulli <fanciulli@gmail.com>
2026-05-25 16:08:27 +0200

    feat: replace Next Admin logo with MusicServer logo and 'Administrator console' title on login

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,6 +1,6 @@
 import Signin from "@/components/Auth/Signin";
+import { Logo } from "@/components/logo";
 import type { Metadata } from "next";
-import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Sign in",
@@ -10,22 +10,9 @@ export default function SignIn() {
   return (
     <div className="w-full max-w-[480px] rounded-2xl bg-white p-8 shadow-2xl dark:bg-gray-dark">
       <div className="mb-8 flex flex-col items-center gap-3">
-        <Image
-          src={"/images/logo/logo-dark.svg"}
-          alt="Logo"
-          width={140}
-          height={28}
-          className="dark:hidden"
-        />
-        <Image
-          src={"/images/logo/logo.svg"}
-          alt="Logo"
-          width={140}
-          height={28}
-          className="hidden dark:block"
-        />
+        <Logo />
         <h1 className="text-xl font-bold text-dark dark:text-white">
-          Admin Login
+          Administrator console
         </h1>
       </div>
 

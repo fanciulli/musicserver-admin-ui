bdf8516a938636adde03dfb54920e52e3846e3ea
Massimiliano Fanciulli <fanciulli@gmail.com>
2026-05-25 17:49:19 +0200

    Add icon-only logo variant and use it in login modal

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,6 +1,5 @@
 import Signin from "@/components/Auth/Signin";
-import darkLogo from "@/assets/logos/dark.svg";
-import logo from "@/assets/logos/main.svg";
+import logoIcon from "@/assets/logos/icon.svg";
 import type { Metadata } from "next";
 import Image from "next/image";
 
@@ -13,19 +12,10 @@ export default function SignIn() {
     <div className="w-full max-w-[480px] rounded-2xl bg-white p-8 shadow-2xl dark:bg-gray-dark">
       <div className="mb-8 flex flex-col items-center gap-4">
         <Image
-          src={logo}
-          width={174}
-          height={30}
+          src={logoIcon}
+          width={40}
+          height={38}
           alt="Music Server"
-          className="dark:hidden"
-          priority
-        />
-        <Image
-          src={darkLogo}
-          width={174}
-          height={30}
-          alt="Music Server"
-          className="hidden dark:block"
           priority
         />
         <h1 className="text-xl font-bold text-dark dark:text-white">
